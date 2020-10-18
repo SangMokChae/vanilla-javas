e6442fd38c9eb6552ff05a594d9fa8f6c76ce723
SangMokChae <forhs13@naver.com>
2020-10-18 19:07:33 +0900

    started__js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('Im Working. Im JS. Im Beautiful. Im Worth it');
+const loveNumber = [1, 2, 3, 4, 5, 6]
+console.log(loveNumber[3]);
+
+const mokInfo = {
+  name: "Mok",
+  age: 28,
+  gender: "Male",
+  favThings: ["readComics", "playTheGame", "listeningToMusic"],
+  whatKindaThings: [
+    {
+      readComics: "Tonikaku",
+      howMuch: "30minutes"
+    },
+    {
+      playTheGame: "Lol",
+      howMuch: "1hour"
+    }
+  ]
+}
+
+console.log(mokInfo.whatKindaThings[1]);
