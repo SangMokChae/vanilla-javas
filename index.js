5fd8e25dad72b6b6ffe0dd1c6958d2237fff293f
SangMokChae <forhs13@naver.com>
2020-10-19 19:41:10 +0900

    used console.dir and getElementbyid

--- index.js
+++ index.js
@@ -1,27 +1,5 @@
-const a = 2, b = 4, c = 6 
-
-const calculator = {
-  plus: function(a, b, c) {
-    return a + b + c;
-  },
-  minus: function(a, b, c) {
-    return a - b - c;
-  },
-  multiply: function(a, b, c) {
-    return a * b * c;
-  },
-  divide: function(a, b, c) {
-    return a / b / c;
-  },
-  square: function(a, b, c) {
-    return (a ** b) ** c;
-  }
-}
-
-const plus = calculator.plus(a, b, c);
-const minus = calculator.minus(a, b, c);
-const multiply = calculator.multiply(a, b, c);
-const divide = calculator.divide(a, b, c);
-const square = calculator.square(a, b, c);
-
-console.log(`plus: ${plus}, minus: ${minus}, multiply: ${multiply}, divide: ${divide}, square: ${square}.`)
+const title = document.getElementById("sori");
+sori.innerHTML = ("Hello Ruby!");
+sori.style.color = ("yellowgreen");
+sori.style.backgroundColor = ("blue");
+console.dir(title);
