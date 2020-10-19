dea3219a41f8ff72db69a70c287320b2f8d1588c
SangMokChae <forhs13@naver.com>
2020-10-19 23:21:44 +0900

    if_else with colorchange

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 const title = document.querySelector("#title");
 
-const BASE_COLOR = "#34495e";
+const BASE_COLOR = "rgb(230, 126, 34)";
+const OTHER_COLOR = "#3498db";
+
+console.log(title.style.color);
 
 function handleClick() {
-  console.log(title.style.color);
+  const currentColor = title.style.color;
+  if (currentColor === BASE_COLOR) {
+    title.style.color = OTHER_COLOR;
+  } else {
+    title.style.color = BASE_COLOR;
+  }
 }
 
 function init() {
   title.style.color = BASE_COLOR;
-  title.addEventListener("click", handleClick);
+  title.addEventListener("mouseenter", handleClick);
 }
 init();
+
+function handleFocus() {
+  title.style.color = "#ecf0f1";
+}
+
+function handleBlur() {
+  title.style.color = "#2c3e50";
+}
+
+window.addEventListener("focus", handleFocus);
+window.addEventListener("blur", handleBlur);
